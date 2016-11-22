44b38d7a7fb42c51579ed0e7f6bb23e01d8374ce
Romel D Mackelprang <romelm@u.washington.edu>
2016-11-22 13:14:41 -0800

    Add SeattleDataFolder

---
+++ SeattleWeatherData/WriteMd.R
@@ -0,0 +1,22 @@
+file='SeattleWeatherData.rmd'
+
+name=gsub('.rmd', '', file)
+name=paste(Sys.Date(), '-', name, sep='')
+out.file=paste(name, '.md',sep='')
+fig.file=paste(name, '-figures/',sep='')
+
+knitr::opts_knit$set(base.url = "/")
+knitr::opts_chunk$set(fig.path = fig.file)
+
+
+
+
+content <- readLines(file)
+
+knitr::knit(text=content, output=out.file)
+
+file.copy(from=out.file, to='../_posts')
+file.copy(from=fig.file, to='../images', recursive=TRUE)
+
+file.remove(out.file)
+file.remove(fig.file, recursive=TRUE)
